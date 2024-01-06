df9d86599f2d528c53c6746ec455dd736f7ee842
Large-Guy <r.lebgue@gmail.com>
2024-01-06 15:16:52 -0800

    Implemented global systems. Put physics into seperate file.

---
+++ src/engine/standard_ecs_physics.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rlebg on 1/6/2024.
+//
+
+#include "standard_ecs_physics.h"
---
+++ src/engine/standard_ecs_physics.h
@@ -0,0 +1,16 @@
+//
+// Created by rlebg on 1/6/2024.
+//
+
+#ifndef POMEGRANATE_ENGINE_STANDARD_ECS_PHYSICS_H
+#define POMEGRANATE_ENGINE_STANDARD_ECS_PHYSICS_H
+
+
+
+class standard_ecs_physics {
+
+};
+
+
+
+#endif //POMEGRANATE_ENGINE_STANDARD_ECS_PHYSICS_H
